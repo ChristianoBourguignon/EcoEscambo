b2e077959924b17f56924219cbf1b0177614f8b6
Natanrezendee <natan.kiffer.magalhaes@outlook.com>
2025-04-04 22:25:03 -0300

    Primeiro commit do projeto EcoEscambo

---
+++ scripts.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    carregarProdutos("trocas_populares", "Trocas Populares");
+    carregarProdutos("adicionados_recentemente", "Adicionados Recentemente");
+    carregarProdutos("todos_os_itens", "Todos os Itens");
+});
