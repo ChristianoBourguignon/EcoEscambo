045553069ee9d5437934bcf389416873a565aca1
ChristianoBourguignon <christianombl@gmail.com>
2025-04-11 18:35:18 -0300

    Ajustando a lógica da operação de troca

--- backend/operacaoTroca.php
+++ backend/operacaoTroca.php
@@ -8,7 +8,9 @@ if (!isset($_SESSION['usuario_id'])) {
 $pdo = Db::getConnection();
 $idUser = $_SESSION['usuario_id'];
 $prodUser = $_POST['meuProd'];
-$prodDesejado = $_POST['prodOferecido'];
+$prodOferecido = $_POST['prodOferecido'];
+$idDonoProdOferecido = '';
+
 $btn = NULL;
 if (isset($_POST['confirmar'])) {
     $btn = true;
@@ -18,14 +20,19 @@ if (isset($_POST['confirmar'])) {
 
 try {
     if($btn){
-        $stmt = $pdo->prepare("SELECT * FROM troca t JOIN produtos p ON p.id IN (t.idProdDesejado, t.idProdUser) WHERE t.idUserDesejado = :idUser;");
-    } else {
-        $stmt = $pdo->prepare("SELECT * FROM troca t JOIN produtos p ON p.id IN (t.idProdDesejado, t.idProdUser) WHERE t.idUserDesejado = :idUser;");
+        $stmt = $pdo->prepare("SELECT * FROM PRODUTOS WHERE ID = :prodOferecido");
+        $stmt->bindParam(':prodOferecido', $prodOferecido);
+        $stmt->execute();
+        $prod = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $idDonoProdOferecido = $prod['idUser'];
+        $stmt = $pdo->prepare("UPDATE produtos SET idUser= :idUser WHERE :prodOferecido");
+
+//        $stmt = $pdo->prepare("SELECT * FROM troca t JOIN produtos p ON p.id IN (t.idProdDesejado, t.idProdUser) WHERE t.idUserDesejado = :idUser;");
+//        $stmt->bindParam(':idUser', $idUser);
+//        $stmt->execute();
+//        $troca = $stmt->fetchAll(PDO::FETCH_ASSOC);
+//        header('Location: ../trocas.php');
     }
-    $stmt->bindParam(':idUser', $idUser);
-    $stmt->execute();
-    $troca = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    header('Location: ../trocas.php');
 
 } catch (PDOException $e) {
     echo "Erro ao buscar produtos: " . $e->getMessage();
