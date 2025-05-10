65d032bcadf79b803c1b81ae61c87e8bb4228bc0
ChristianoBourguignon <christianombl@gmail.com>
2025-05-10 17:39:29 -0300

    feat: add data footers

--- app/views/master.php
+++ app/views/master.php
@@ -25,7 +25,7 @@ $nomeUsuario = $_SESSION['usuario_nome'] ?? null;
         <link rel="stylesheet" href="css/globals.css">
 
         <?php
-        if (stripos($_SERVER['REQUEST_URI'], 'sobre.php')){
+        if ((BASE . "/sobre")){
             ?>
             <!-- CSS sobre -->
             <link rel="stylesheet" href="css/sobre.css">
@@ -42,7 +42,31 @@ $nomeUsuario = $_SESSION['usuario_nome'] ?? null;
 include_once("models/navbarRoter.php");
 
 ?>
+
 <?= $this->section('body') ?>
 
 <?php
-include_once '../../models/footer.php';
+    include_once("perfil.php");
+
+    if ($nomeUsuario) {
+    include_once("models/cadastrarProdutos.php");
+    include_once("models/produtoCadastrado.php");
+
+    if (isset($_SESSION['sucesso'])){ ?>
+    <script>
+        const sucessoModal = new bootstrap.Modal(document.getElementById('modalSucesso'));
+        sucessoModal.show();
+        <?php
+        unset($_SESSION['sucesso']);
+        ?>
+    </script>
+    <?php } else {;?>
+    <script>
+        const modalErro = new bootstrap.Modal(document.getElementById('modalErro'));
+        modalErro.show();
+    </script>
+<?php
+        include_once("models/modalAlterarProduto.php");
+        }
+    }
+?>
