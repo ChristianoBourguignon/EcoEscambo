2d2501fe3c8bc71e021d1816c50417080711464d
ChristianoBourguignon <christianombl@gmail.com>
2025-05-10 17:38:17 -0300

    feat: add new method searchProducts

--- app/views/produtos.php
+++ app/views/produtos.php
@@ -1,59 +1,17 @@
 <?php
+
+namespace app\controllers;
+
 require_once 'backend/db.php';
 $this->layout("master", [
     'title' => "Produtos",
     'description' => "Aqui você encontrará todos os produtos disponível para realizar uma troca, tendo total liberdade de escolha."
 ]);
-
-
-$pdo = Db::getConnection();
 $idUser = $_SESSION['usuario_id'] ?? NULL;
 
+$produtos = (new ProductsController)->buscarProdutos($idUser);
 
-try {
-    if(!$idUser){
-        $stmt = $pdo->prepare("
-        SELECT *
-        FROM produtos p
-        WHERE EXISTS (
-                SELECT 1 
-                FROM troca t 
-                WHERE (t.idProdDesejado = p.id OR t.idProdUser = p.id)
-                  AND t.Status IN (0, -1)
-              )
-          AND NOT EXISTS (
-                SELECT 1 
-                FROM troca t2 
-                WHERE (t2.idProdDesejado = p.id OR t2.idProdUser = p.id)
-                  AND t2.Status = 1
-              );
-        ");
-    } else {
-        $stmt = $pdo->prepare("
-        SELECT *
-        FROM produtos p
-        WHERE p.idUser != :idUser
-          AND EXISTS (
-            SELECT 1 
-            FROM troca t 
-            WHERE (t.idProdDesejado = p.id OR t.idProdUser = p.id)
-              AND t.Status IN (0, -1)
-          )
-          AND NOT EXISTS (
-            SELECT 1 
-            FROM troca t2 
-            WHERE (t2.idProdDesejado = p.id OR t2.idProdUser = p.id)
-              AND t2.Status = 1
-        );
-        ");
-        $stmt->bindParam(':idUser',$idUser);
-    }
-    $stmt->execute();
-    $produtos = $stmt->fetchAll(PDO::FETCH_ASSOC);
-} catch (PDOException $e) {
-    echo "Erro ao buscar produtos: " . $e->getMessage();
-    exit;
-}
+
 
 ?>
 <?php $this->start('body'); ?>
