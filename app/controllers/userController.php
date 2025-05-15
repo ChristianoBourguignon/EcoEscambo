dd654adbc3d9afc4e15216d08746cb92aa7004b5
ChristianoBourguignon <christianombl@gmail.com>
2025-05-15 19:48:34 -0300

    fix: bug products showing user name in dashboard

--- app/controllers/userController.php
+++ app/controllers/userController.php
@@ -89,10 +89,27 @@ class userController
         try {
             dbController::getConnection();
             $stmt = dbController::getPdo()->prepare("
-                SELECT produtos.*, users.nome
-                FROM produtos
-                INNER JOIN users ON produtos.idUser = users.id
-                WHERE produtos.idUser = :idUser;
+                SELECT * FROM produtos p
+                        WHERE p.idUser = :idUser AND
+                              (
+                            EXISTS (
+                                SELECT 1 
+                                FROM troca t 
+                                WHERE (t.idProdDesejado = p.id OR t.idProdUser = p.id)
+                                  AND t.Status IN (0, -1)
+                            )
+                            OR NOT EXISTS (
+                                SELECT 1 
+                                FROM troca t 
+                                WHERE t.idProdDesejado = p.id OR t.idProdUser = p.id
+                            )
+                        )
+                        AND NOT EXISTS (
+                            SELECT 1 
+                            FROM troca t2 
+                            WHERE (t2.idProdDesejado = p.id OR t2.idProdUser = p.id)
+                              AND t2.Status = 1
+                        );
                 "
             );
             $stmt->bindParam(':idUser', $idUser);
