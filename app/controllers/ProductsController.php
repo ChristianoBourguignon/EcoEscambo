f0f040ab03bd8f882560d20eadc8cc1d53a5da70
ChristianoBourguignon <christianombl@gmail.com>
2025-05-15 16:38:03 -0300

    fix: bug products were not being displayed when logged in

--- app/controllers/ProductsController.php
+++ app/controllers/ProductsController.php
@@ -10,49 +10,60 @@ class ProductsController
         Controller::view("produtos");
     }
     public function buscarProdutos($idUser){
-        require_once 'backend/db.php';
-        $pdo = dbController::getConnection();
+        dbController::getConnection();
 
         try {
             if(!$idUser){
                 $stmt = dbController::getPdo()->prepare("
-                    SELECT *
-                    FROM produtos p
-                    WHERE EXISTS (
-                            SELECT 1 
-                            FROM troca t 
-                            WHERE (t.idProdDesejado = p.id OR t.idProdUser = p.id)
-                              AND t.Status IN (0, -1)
-                          )
-                      AND NOT EXISTS (
+                    SELECT * FROM produtos p
+                        WHERE (
+                            EXISTS (
+                                SELECT 1 
+                                FROM troca t 
+                                WHERE (t.idProdDesejado = p.id OR t.idProdUser = p.id)
+                                  AND t.Status IN (0, -1)
+                            )
+                            OR NOT EXISTS (
+                                SELECT 1 
+                                FROM troca t 
+                                WHERE t.idProdDesejado = p.id OR t.idProdUser = p.id
+                            )
+                        )
+                        AND NOT EXISTS (
                             SELECT 1 
                             FROM troca t2 
                             WHERE (t2.idProdDesejado = p.id OR t2.idProdUser = p.id)
                               AND t2.Status = 1
-                          );
+                        );
                     ");
             } else {
                 $stmt = dbController::getPdo()->prepare("
-                    SELECT *
-                    FROM produtos p
-                    WHERE p.idUser != :idUser
-                      AND EXISTS (
-                        SELECT 1 
-                        FROM troca t 
-                        WHERE (t.idProdDesejado = p.id OR t.idProdUser = p.id)
-                          AND t.Status IN (0, -1)
-                      )
-                      AND NOT EXISTS (
-                        SELECT 1 
-                        FROM troca t2 
-                        WHERE (t2.idProdDesejado = p.id OR t2.idProdUser = p.id)
-                          AND t2.Status = 1
-                    );
+                    SELECT * FROM produtos p
+                        WHERE p.idUser != :idUser AND
+                              (
+                            EXISTS (
+                                SELECT 1 
+                                FROM troca t 
+                                WHERE (t.idProdDesejado = p.id OR t.idProdUser = p.id)
+                                  AND t.Status IN (0, -1)
+                            )
+                            OR NOT EXISTS (
+                                SELECT 1 
+                                FROM troca t 
+                                WHERE t.idProdDesejado = p.id OR t.idProdUser = p.id
+                            )
+                        )
+                        AND NOT EXISTS (
+                            SELECT 1 
+                            FROM troca t2 
+                            WHERE (t2.idProdDesejado = p.id OR t2.idProdUser = p.id)
+                              AND t2.Status = 1
+                        );
                 ");
                 $stmt->bindParam(':idUser',$idUser);
             }
             $stmt->execute();
-            $produtos = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            $produtos = $stmt->fetchAll(dbController::getPdo()::FETCH_ASSOC);
             return $produtos;
         } catch (PDOException $e) {
             echo "Erro ao buscar produtos: " . $e->getMessage();
