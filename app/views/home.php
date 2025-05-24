7b41649f70a52f86976a3f292fe12172c3a50038
ChristianoBourguignon <christianombl@gmail.com>
2025-05-24 12:37:35 -0300

    fix: direct link in home page and sql dialects in intellij

--- app/views/home.php
+++ app/views/home.php
@@ -11,7 +11,7 @@
         <div class="line"></div>
         <h1 class="fade-in">O <span class="Futuro">EcoEscambo</span> é o Futuro do Consumo Consciente</h1>
         <p class="fade-in delay">Dê uma nova vida aos seus produtos e descubra novos tesouros sem gastar nada!</p>
-        <a href="produtos.php" class="vrit fade-in delay">Ver Itens</a>
+        <a href="<?= BASE ?>/produtos" class="vrit fade-in delay">Ver Itens</a>
     </div>
 </header>
 
